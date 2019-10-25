394771c60c6d36348d0eae20c651662cbea03882
Levi Sørum <levi.sorum@gmail.com>
2019-10-25 15:52:21 +0200

    Added navigation in game.

--- game.js
+++ game.js
@@ -1,30 +1,54 @@
-
-var exit = false;
 var isPlaying = false;
 
+$('#playButton').click(function(e){
+    showPlay();
+});
+$('#endButton').click(function(e){
+    showFinish();
+});
+$('#retryButton').click(function(e){
+    showPlay();
+});
+$('#quitButton').click(function(e){
+    showMenu();
+});
+
+function update(progress) {
+    // Update the state of the world for the elapsed time since last render
+  }
+  
+function draw() {
+    // Draw the state of the world
+}
+
 var enterGame = function ()  {
-// Wait for the page to finish loading
-console.log("enter game")
-
-        $('button#playButton').click(function(e){
-            console.log("Playbutton");
-        });
-        $('#retryButton').click(function(e){
-            console.log("button");
-        });
-        $('#quitButton').click(function(e){
-            console.log("button");
-        });
-        
-        function update(progress) {
-            // Update the state of the world for the elapsed time since last render
-          }
-          
-          function draw() {
-            // Draw the state of the world
-          }
+    showMenu();
 }
-
 var exitGame = function (){
-    console.log("exit game");
+    hideAll();
 }
+var showMenu = function(){
+    $('#menu').show();
+    $('#play').hide();
+    $('#finish').hide();
+    isPlaying = false;
+}
+var showPlay = function(){
+    $('#menu').hide();
+    $('#play').show();
+    $('#finish').hide();
+    isPlaying = true;
+}
+var showFinish = function(){
+    $('#menu').hide();
+    $('#play').hide();
+    $('#finish').show();
+    isPlaying = false;
+}
+var hideAll = function(){
+    $('#menu').hide();
+    $('#play').hide();
+    $('#finish').hide();
+    isPlaying = false;
+}
+
