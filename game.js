2384ca4a25c0a1f650d16f1f8aa92bcfa09060db
Levi Sørum <levi.sorum@gmail.com>
2019-11-01 08:56:37 +0100

    Added input

--- game.js
+++ game.js
@@ -63,6 +63,29 @@ var resetGame = function() {
 }
 
 
+$(document).on("keypress", "#game", function (e) {
+    const key = e.key.toLowerCase();
+    if(key >= "a" && key <= "z"){
+        checkLetter(key);
+    }
+});
+
+var checkLetter = function(letter){
+    if(correctLetters.includes(letter) || wrongLetters.includes(letter)){
+        return;
+    }
+    for(const c of "hello world"){
+        if(letter === c){
+            correctLetters.push(letter);
+            return;
+        }
+    }
+    wrongLetters.push(letter);
+    return;
+}
+
+
+
 // Navigation
 
 var enterGame = function ()  {
