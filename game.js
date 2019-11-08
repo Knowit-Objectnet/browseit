cc3cbdbfb9bba8e402f7c0b0bcae9e5f44a1e0d7
haattis <exburn0@gmail.com>
2019-11-08 13:17:05 +0100

    prettier

--- game.js
+++ game.js
@@ -2,50 +2,47 @@ var acceptInput = true;
 var isPlaying = false;
 var highScore = 0;
 
-$('#playButton').click(function(e){
+$("#playButton").click(function(e) {
   showPlay();
   initGame();
   isPlaying = true;
 });
-$('#endButton').click(function(e){
+$("#endButton").click(function(e) {
   if (acceptInput) {
     isPlaying = false;
     gameOver();
   }
 });
-$('#retryButton').click(function(e){
+$("#retryButton").click(function(e) {
   initGame();
   showPlay();
   isPlaying = true;
 });
-$('#quitButton').click(function(e){
+$("#quitButton").click(function(e) {
   isPlaying = false;
   showMenu();
 });
 
-
-var getLocalHighScore = function(){
+// Local Storage
+var getLocalHighScore = function() {
   highScore = localStorage.getItem("browseItNameGameScore");
-  if(highScore == null){
+  if (highScore == null) {
     highScore = 0;
     $("#bestHighScore").html("");
-  }
-  else{
+  } else {
     $("#bestHighScore").html("Din beste poengsum: " + highScore);
   }
-}
+};
 
-var checkIfNewHighScore = function (){
-  if(score > highScore){
+var checkIfNewHighScore = function() {
+  if (score > highScore) {
     highScore = score;
     localStorage.setItem("browseItNameGameScore", score);
   }
-}
+};
 
-
-// Game logic
+// Game state
 var employees = [];
-
 var lives = 15;
 var score = 0;
 var employeesLeft = [];
@@ -57,56 +54,40 @@ var initGame = function() {
   resetGame();
   changePerson();
   renderGameState();
-}
+};
 
-var changePerson = function(){
+var changePerson = function() {
   wrongLetters = [];
   correctLetters = [];
-  let person = employeesLeft.splice(Math.floor(Math.random() * employeesLeft.length), 1)[0];
+  let person = employeesLeft.splice(
+    Math.floor(Math.random() * employeesLeft.length),
+    1
+  )[0];
   currentName = person.name;
 
   // image
-  $('#personImage').attr("src", person.img);
+  $("#personImage").attr("src", person.img);
 
   // letters
-  let letters = person.name.split('').map(letter => {
+  let letters = person.name.split("").map(letter => {
     let id;
     let className;
     let hidden;
-    if (letter === ' ') {
-      className = 'symbol';
-    } else if (letter === '-') {
-      className = 'symbol';
+    if (letter === " ") {
+      className = "symbol";
+    } else if (letter === "-") {
+      className = "symbol";
     } else {
       id = letter.toLowerCase();
-      className = 'letterContainer';
+      className = "letterContainer";
       hidden = true;
-    };
-    return `<div class="${className}"><div class="${hidden ? 'hidden' : ''} letter ${id ? id : ''}">${letter.toUpperCase()}</div></div>`;
+    }
+    return `<div class="${className}"><div class="${
+      hidden ? "hidden" : ""
+    } letter ${id ? id : ""}">${letter.toUpperCase()}</div></div>`;
   });
-  $('#nameSection').append(letters);
-
+  $("#nameSection").append(letters);
 };
-
-var showWrongLetter = function(letter){
-  $('#alphabet').append(`<div class="letterContainer wrong"><div class="letter ${letter}">${letter.toUpperCase()}</div></div>`);
-};
-
-var showCorrectLetter = function(letter) {
-  $(`.letter.${letter.toLowerCase()}`).removeClass('hidden');
-};
-
-var showCorrectName = function() {
-  $('#nameSection .letterContainer').addClass('isCorrectName');
-  $('#nameSection .letterContainer').addClass('no-underline');
-};
-
-var showWrongName = function() {
-  $('.letter.hidden').css("font-weight", "bold");
-  $(`.letter.hidden`).addClass('isWrongName');
-  $(`.letter.hidden`).removeClass('hidden');
-  $('#nameSection .letterContainer').addClass('no-underline');
-}
 
 var incrementScore = function() {
   score += 1;
@@ -115,63 +96,6 @@ var incrementScore = function() {
 var decrementLives = function() {
   lives -= 1;
 };
-
-var updateGameState = function(){
-  if(lives <= 0){
-    $('#endButton').removeClass('red');
-    acceptInput = false;
-    showWrongName();
-    setTimeout(function() {
-      gameOver();
-      acceptInput = true;
-      $('#endButton').addClass('red');
-    }, 4000);
-  } else {
-    var win = true;
-    var nameIter = currentName.toLowerCase().split(" ").join("").split("-").join("");
-    for(const c of nameIter){
-      if(!correctLetters.includes(c)){
-        win = false;
-        break;
-      }
-    }
-    if(win){
-      $('#endButton').removeClass('red');
-      showCorrectName();
-      incrementScore();
-      acceptInput = false;
-      setTimeout(() => {
-        if(employeesLeft.length === 0){
-          gameWin();
-        }
-        else{
-          roundWin();
-        }
-        $('#endButton').addClass('red');
-        acceptInput = true;
-      }, 2500);
-    }
-  }
-}
-
-var roundWin = function (){
-  resetGameRender();
-  changePerson();
-}
-
-var gameOver = function (){
-  showFinish();
-  checkIfNewHighScore();
-  isPlaying = false;
-  renderFinish();
-}
-
-var gameWin = function (){
-  showFinish();
-  checkIfNewHighScore();
-  isPlaying = false;
-  renderFinish();
-}
 
 var resetGame = function() {
   score = 0;
@@ -182,57 +106,13 @@ var resetGame = function() {
   resetGameRender();
 };
 
-var resetGameRender = function() {
-  $("#nameSection").empty();
-  $("#alphabet").empty();
-}
-
-var resetImageRender = function (){
-  $('#personImage').attr("src", "");
-}
-
-var renderGameState = function (){
-  $("#scoreLabel").html("Poeng: "+score);
-  $("#livesLabel").html("Antall liv: "+lives);
-}
-
-var renderFinish = function (win){
-  if(win){
-    $("#finishTitle").html("Gratulerer, du vant!")
-  }
-  else{
-    $("#finishTitle").html("Du tapte!");
-  }
-  $("#score").html(score + " av " + employees.length);
-  $("#highScore").html("Din rekord: " + highScore);
-}
-
-
-$(document).on("keypress", window, function (e) {
-  if(acceptInput){
-    const key = e.key.toLowerCase();
-    if(((key >= "a" && key <= "z") || "æøå".includes(key)) && e.keyCode != 13){
-      checkLetter(key);
-      updateGameState();
-      renderGameState();
-    }
-  }
-});
-
-var scaleLetter = function(letter) {
-  $(`.${letter}`).addClass('letter-scale');
-  setTimeout(function() {
-    $(`.${letter}`).removeClass('letter-scale');
-  }, 200)
-}
-
-var checkLetter = function(letter){
-  if(correctLetters.includes(letter) || wrongLetters.includes(letter)){
+var checkLetter = function(letter) {
+  if (correctLetters.includes(letter) || wrongLetters.includes(letter)) {
     scaleLetter(letter);
     return;
   }
-  for(const c of currentName.toLowerCase()){
-    if(letter === c){
+  for (const c of currentName.toLowerCase()) {
+    if (letter === c) {
       correctLetters.push(letter);
       showCorrectLetter(letter);
       return;
@@ -242,57 +122,187 @@ var checkLetter = function(letter){
   showWrongLetter(letter);
   decrementLives();
   return;
-}
+};
 
+var updateGameState = function() {
+  if (lives <= 0) {
+    $("#endButton").removeClass("red");
+    acceptInput = false;
+    showWrongName();
+    setTimeout(function() {
+      gameOver();
+      acceptInput = true;
+      $("#endButton").addClass("red");
+    }, 4000);
+  } else {
+    var win = true;
+    var nameIter = currentName
+      .toLowerCase()
+      .split(" ")
+      .join("")
+      .split("-")
+      .join("");
+    for (const c of nameIter) {
+      if (!correctLetters.includes(c)) {
+        win = false;
+        break;
+      }
+    }
+    if (win) {
+      $("#endButton").removeClass("red");
+      showCorrectName();
+      incrementScore();
+      acceptInput = false;
+      setTimeout(() => {
+        if (employeesLeft.length === 0) {
+          gameWin();
+        } else {
+          roundWin();
+        }
+        $("#endButton").addClass("red");
+        acceptInput = true;
+      }, 2500);
+    }
+  }
+};
 
+// Game navigation
+var roundWin = function() {
+  resetGameRender();
+  changePerson();
+};
 
-// Navigation
+var gameOver = function() {
+  showFinish();
+  checkIfNewHighScore();
+  isPlaying = false;
+  renderFinish();
+};
 
-var enterGame = function ()  {
+var gameWin = function() {
+  showFinish();
+  checkIfNewHighScore();
+  isPlaying = false;
+  renderFinish();
+};
+
+var enterGame = function() {
   showMenu();
   getLocalHighScore();
   loadNames();
-}
-var loadNames = function () {
+};
+
+// Event listener
+$(document).on("keypress", window, function(e) {
+  if (acceptInput) {
+    const key = e.key.toLowerCase();
+    if (
+      ((key >= "a" && key <= "z") || "æøå".includes(key)) &&
+      e.keyCode != 13
+    ) {
+      checkLetter(key);
+      updateGameState();
+      renderGameState();
+    }
+  }
+});
+
+// Render functions
+var showWrongLetter = function(letter) {
+  $("#alphabet").append(
+    `<div class="letterContainer wrong"><div class="letter ${letter}">${letter.toUpperCase()}</div></div>`
+  );
+};
+
+var showCorrectLetter = function(letter) {
+  $(`.letter.${letter.toLowerCase()}`).removeClass("hidden");
+};
+
+var showCorrectName = function() {
+  $("#nameSection .letterContainer").addClass("isCorrectName");
+  $("#nameSection .letterContainer").addClass("no-underline");
+};
+
+var showWrongName = function() {
+  $(".letter.hidden").css("font-weight", "bold");
+  $(`.letter.hidden`).addClass("isWrongName");
+  $(`.letter.hidden`).removeClass("hidden");
+  $("#nameSection .letterContainer").addClass("no-underline");
+};
+
+var scaleLetter = function(letter) {
+  $(`.${letter}`).addClass("letter-scale");
+  setTimeout(function() {
+    $(`.${letter}`).removeClass("letter-scale");
+  }, 200);
+};
+
+var resetGameRender = function() {
+  $("#nameSection").empty();
+  $("#alphabet").empty();
+};
+
+var resetImageRender = function() {
+  $("#personImage").attr("src", "");
+};
+
+var renderGameState = function() {
+  $("#scoreLabel").html("Poeng: " + score);
+  $("#livesLabel").html("Antall liv: " + lives);
+};
+
+var renderFinish = function(win) {
+  if (win) {
+    $("#finishTitle").html("Gratulerer, du vant!");
+  } else {
+    $("#finishTitle").html("Du tapte!");
+  }
+  $("#score").html(score + " av " + employees.length);
+  $("#highScore").html("Din rekord: " + highScore);
+};
+
+var loadNames = function() {
   $("#loading").show();
   $("#loadingFinished").hide();
   $("#loadingError").hide();
   getEmployees()
-  .then((result) => {
-    employees = result;
-    employeesLeft = result;
-    $("#loading").hide();
-    $("#loadingFinished").show();
-    $("#loadingError").hide();
-  }).catch((e) => {
-    $("#loading").hide();
-    $("#loadingFinished").hide();
-    $("#loadingError").show();
-    $("#loadingError").html("Feil ved innlasting av spill. Sørg for at du er logget inn på projects.knowit.no, og last inn spillet på nytt.");
-  });
-}
-var exitGame = function (){
+    .then(result => {
+      employees = result;
+      employeesLeft = result;
+      $("#loading").hide();
+      $("#loadingFinished").show();
+      $("#loadingError").hide();
+    })
+    .catch(e => {
+      $("#loading").hide();
+      $("#loadingFinished").hide();
+      $("#loadingError").show();
+      $("#loadingError").html(
+        "Feil ved innlasting av spill. Sørg for at du er logget inn på projects.knowit.no, og last inn spillet på nytt."
+      );
+    });
+};
+var exitGame = function() {
   hideAll();
-}
-var showMenu = function(){
-  $('#menu').show();
-  $('#play').hide();
-  $('#finish').hide();
-}
-var showPlay = function(){
-  $('#menu').hide();
-  $('#play').show();
-  $('#finish').hide();
+};
+var showMenu = function() {
+  $("#menu").show();
+  $("#play").hide();
+  $("#finish").hide();
+};
+var showPlay = function() {
+  $("#menu").hide();
+  $("#play").show();
+  $("#finish").hide();
   $("#play").focus();
-}
-var showFinish = function(){
-  $('#menu').hide();
-  $('#play').hide();
-  $('#finish').show();
-}
-var hideAll = function(){
-  $('#menu').hide();
-  $('#play').hide();
-  $('#finish').hide();
-}
-
+};
+var showFinish = function() {
+  $("#menu").hide();
+  $("#play").hide();
+  $("#finish").show();
+};
+var hideAll = function() {
+  $("#menu").hide();
+  $("#play").hide();
+  $("#finish").hide();
+};
