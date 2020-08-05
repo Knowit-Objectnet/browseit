c21425e0a1e0fb6c9d8fb0777ede50bdaba6d5d0
Alireza Balouch <gswape@gmail.com>
2020-08-05 15:34:15 +0200

    cleaned up naming for keys

--- src/components/LinkSection.js
+++ src/components/LinkSection.js
@@ -3,15 +3,15 @@ import React from "react";
 export function Links({ link_sections: linkSections}) {
   return (
     <div id="home">
-      {linkSections.map((section, i) => {
+      {linkSections.map((section, linkSectionKey) => {
         return (
-          <section className={section.class} key={i}>
+          <section className={section.class} key={linkSectionKey}>
             <h1>{section.header}</h1>
             <table>
               <tbody>
-                {section.links.map((l, i) => {
+                {section.links.map((l, key) => {
                   return (
-                    <tr key={i}>
+                    <tr key={key}>
                       <td className={section.title_class}>{l.title}</td>
                       <td className={section.href_class}>
                         <a href={l.href}>{l.href_title}</a>
