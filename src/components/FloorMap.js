d79db136b8266126fc6d026ee6b225bf464f9a0f
Alireza Balouch <gswape@gmail.com>
2020-08-05 15:21:27 +0200

    using destructor to get the props we need and simplify style

--- src/components/FloorMap.js
+++ src/components/FloorMap.js
@@ -1,23 +1,22 @@
 import React from "react";
 import fifthFloor from "../img/sundt-etasje-5.png";
 
-export function FloorMap(props) {
-  let visible = props.showMap;
+export function FloorMap({showMap, setShowMap}) {
+
   const style = {
-    map: {
-        paddingTop: "1.5em",
+      paddingTop: "1.5em",
       width: "100%",
       cursor: "pointer"
-    }
   };
+
   return (
     <div>
       <img
         src={fifthFloor}
-        style={style.map}
+        style={style}
         title="click to close"
         alt=""
-        onClick={() => props.setShowMap(!visible)}
+        onClick={() => setShowMap(!showMap)}
       />
     </div>
   );
