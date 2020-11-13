7d56c6aa2ac529debef9f562ccbe6021f534e5d1
Tom Martin Roberg <tom.martin.roberg@knowit.no>
2020-11-13 11:51:15 +0100

    Added objectnet-trondheim.json

--- src/json/index.js
+++ src/json/index.js
@@ -1,2 +1,3 @@
 export { default as amendeJson } from './amende.json';
 export { default as objectNetJson } from './objectnet.json';
+export { default as objectNetTrondheimJson } from './objectnet-trondheim.json';
