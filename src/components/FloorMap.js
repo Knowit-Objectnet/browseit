d4abfd2488f913c4ad6ff50713c27d7d12f27fd8
Arvin Khodabandeh <arvin.khodabandeh@knowit.no>
2023-02-03 14:48:45 +0100

    FIX: Updated maps for the three floors in Universitetsgata 1

--- src/components/FloorMap.js
+++ src/components/FloorMap.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import fifthFloor from '../img/sundt-etasje-5.png';
+import React, { useState } from 'react';
+import { ReactComponent as ThirdFloor } from '../img/u7-map-3.svg';
+import { ReactComponent as FourthFloor } from '../img/u7-map-4.svg';
+import { ReactComponent as FifthFloor } from '../img/u7-map-5.svg';
 
 export function FloorMap({ showMap, setShowMap }) {
   const style = {
@@ -8,15 +10,61 @@ export function FloorMap({ showMap, setShowMap }) {
     cursor: 'pointer',
   };
 
+  const thirdFloor = (
+    <ThirdFloor
+      fill="darkseagreen"
+      stroke="black"
+      strokeWidth={0.5}
+      title="click to close"
+      alt=""
+      onClick={() => setShowMap(!showMap)}
+    />
+  );
+
+  const fourthFloor = (
+    <FourthFloor
+      fill="darkseagreen"
+      stroke="black"
+      strokeWidth={0.5}
+      title="click to close"
+      alt=""
+      onClick={() => setShowMap(!showMap)}
+    />
+  );
+
+  const fifthFloor = (
+    <FifthFloor
+      fill="darkseagreen"
+      stroke="black"
+      strokeWidth={0.5}
+      title="click to close"
+      alt=""
+      onClick={() => setShowMap(!showMap)}
+    />
+  );
+
+  const floors = {
+    thirdFloor: thirdFloor,
+    fourthFloor: fourthFloor,
+    fifthFloor: fifthFloor,
+  };
+
+  const [selectedFloorComponent, setSelectedFloorComponent] = useState(
+    floors['fifthFloor']
+  );
+
+  const handleSelectedFloor = (event) => {
+    setSelectedFloorComponent(floors[event.target.value]);
+  };
+
   return (
     <div>
-      <img
-        src={fifthFloor}
-        style={style}
-        title="click to close"
-        alt=""
-        onClick={() => setShowMap(!showMap)}
-      />
+      <select onChange={handleSelectedFloor}>
+        <option value={'fifthFloor'}>Femte etasje U1</option>
+        <option value={'fourthFloor'}>Fjerde etasje U1</option>
+        <option value={'thirdFloor'}>Tredje etasje U1</option>
+      </select>
+      <div style={style}>{selectedFloorComponent}</div>
     </div>
   );
 }
